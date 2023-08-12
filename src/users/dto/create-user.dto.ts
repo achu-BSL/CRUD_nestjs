03d41045f26594fa824f96201b4c6022e192d6b7
achu-bsl <asharudeenalikt@gmail.com>
2023-08-12 20:31:00 +0530

    validation added

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, Max, Min, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @MinLength(3)
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @Min(0)
+    @Max(100)
     age: number;
 } 
